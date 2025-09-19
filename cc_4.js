cf827bfaac5e28921a8f66e870d11308668be702
Joshua Glaser <jdglaser@usf.edu>
2025-09-19 14:15:30 -0400

    First commit, includes up to step 2

---
+++ cc_4.js
@@ -0,0 +1,34 @@
+// Step 1 is to create the .js and html file
+
+// Step 2 building the array
+let products = [{
+    name: "Speakers",
+    category: "electronics",
+    price: 49.99,
+    inventory: 25,
+},
+{
+    name: "T-Shirt",
+    category: "apparel",
+    price: 9.99,
+    inventory: 30,
+},
+{
+    name: "Milk",
+    category: "groceries",
+    price: 3.99,
+    inventory: 120,
+},
+{
+    name: "Lamp",
+    category: "household",
+    price: 29.99,
+    inventory: 20,
+},
+{
+    name: "Advil",
+    category: "medical",
+    price: 11.99,
+    inventory: 20,
+},
+]
